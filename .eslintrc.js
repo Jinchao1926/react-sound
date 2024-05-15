f833735e2471f3a3949e71e78994d802a48743ea
Jinchao Lin <jinchao@fireworkhq.com>
2024-05-15 22:49:07 +0800

    [chore] - Update ESLint + Prettier configurations [chore] - Add husky + lint-staged configurations

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  extends: ['react-app', 'react-app/jest', 'plugin:prettier/recommended'],
+  plugins: ['import'],
+  rules: {
+    eqeqeq: ['error', 'always'],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'no-console': 'error',
+    'no-unused-vars': [
+      'error',
+      {
+        ignoreRestSiblings: true,
+      },
+    ],
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'all',
+      },
+    ],
+  },
+}
