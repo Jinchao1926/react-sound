385881f9dcdd53bcc413212004511f0c590e348d
Jinchao Lin <51687782+Jinchao1926@users.noreply.github.com>
2025-11-04 09:29:12 +0800

    [Cleanup] Refactor download component (#47)

--- src/modules/Detail/components/MultiDownload/MultiDownload.tsx
+++ src/modules/Detail/components/MultiDownload/MultiDownload.tsx
@@ -1,34 +1,22 @@
 import type { FC } from 'react'
 
 import { SectionHeader } from '@/components/SectionHeader'
+import { Box, Flex, Text } from '@/components/UI'
 
-import { MultiDownloadWrapper } from './MultiDownload.styles'
+import { Android, IOS, PC } from './MultiDownload.styles'
 
 export const MultiDownload: FC = () => {
   return (
-    <MultiDownloadWrapper>
+    <Box mb={24}>
       <SectionHeader variant="simple" title="网易云音乐多端下载" />
-      <div className="list sprite_download">
-        <a
-          className="iOS sprite_download"
-          href="https://itunes.apple.com/cn/app/id590338362"
-        >
-          iPhone
-        </a>
-        <a
-          className="pc sprite_download"
-          href="https://music.163.com/api/pc/download/latest"
-        >
-          PC
-        </a>
-        <a
-          className="android sprite_download"
-          href="https://music.163.com/api/android/download/latest2"
-        >
+      <Flex justify="space-between" height={65} mt={20} mb={10}>
+        <IOS href="https://itunes.apple.com/cn/app/id590338362">iPhone</IOS>
+        <PC href="https://music.163.com/api/pc/download/latest">PC</PC>
+        <Android href="https://music.163.com/api/android/download/latest2">
           Android
-        </a>
-      </div>
-      <span className="tip">同步歌单，随时畅听320k好音乐</span>
-    </MultiDownloadWrapper>
+        </Android>
+      </Flex>
+      <Text color="#999">同步歌单，随时畅听320k好音乐</Text>
+    </Box>
   )
 }
