62a39877da4c1159bf16bdeb07231446566236bc
Jinchao Lin <51687782+Jinchao1926@users.noreply.github.com>
2025-11-15 17:12:09 +0800

    [Cleanup] Refactor playlist category (#56)

--- src/modules/Discover/pages/Playlist/components/PlaylistCategory/PlaylistCategory.styles.ts
+++ src/modules/Discover/pages/Playlist/components/PlaylistCategory/PlaylistCategory.styles.ts
@@ -1,122 +1,108 @@
 import styled from 'styled-components'
 
+import { TextNavLink } from '@/components/UI'
+import { Sprite } from '@/components/UI/Spirit'
+
 export const PlaylistCategoryWrapper = styled.div`
   position: absolute;
   z-index: 5;
   top: 35px;
   left: -40px;
   width: 720px;
+`
 
-  .header {
-    height: 32px;
-    background-position: 0 0;
-    .arrow {
-      position: absolute;
-      top: 2px;
-      left: 132px;
-      width: 24px;
-      height: 11px;
-      background-position: -48px 0;
-    }
-  }
-  .body {
-    background-position: -720px 0;
-    background-repeat: repeat-y;
-    padding: 0 10px;
+export const PlaylistCategoryArrow = styled(Sprite).attrs({
+  sprite: 'icon',
+  icon: 'arrow',
+})`
+  position: absolute;
+  top: 2px;
+  left: 132px;
+  width: 24px;
+  height: 11px;
+`
 
-    .all {
-      height: 37px;
-      padding-left: 26px;
-      border-bottom: 1px solid #e6e6e6;
+export const PlaylistCategoryHeader = styled(Sprite).attrs({
+  sprite: 'background',
+  icon: 'header',
+})`
+  height: 32px;
+`
 
-      & > button,
-      & > a {
-        background-position: 0 -64px;
-        width: 75px;
-        height: 26px;
-        cursor: pointer;
-        &:hover {
-          text-decoration: underline;
-        }
-      }
+export const PlaylistCategoryBody = styled(Sprite).attrs({
+  sprite: 'background',
+  icon: 'body',
+})`
+  padding: 0 10px;
 
-      & > a {
-        display: inline-block;
-        line-height: 26px;
-        text-align: center;
-      }
-    }
-
-    dl {
-      &:last-of-type {
-        dd {
-          padding-bottom: 25px;
-        }
-      }
-      dt {
-        float: left;
-        display: inline-flex;
-        align-items: center;
-        width: 70px;
-        margin: 0 -100px 0 26px;
-        padding-top: 15px;
-        /* border-right: 1px solid #e6e6e6; */
-        font-weight: bold;
-
-        .icon {
-          display: inline-block;
-          width: 23px;
-          height: 23px;
-          margin: 0 8px 4px 0;
-        }
-        .icon0 {
-          background-position: -20px -735px;
-        }
-        .icon1 {
-          background-position: 0 -60px;
-        }
-        .icon2 {
-          background-position: 0 -88px;
-        }
-        .icon3 {
-          background-position: 0 -117px;
-        }
-        .icon4 {
-          background-position: 0 -141px;
-        }
-      }
-
+  dl {
+    margin: 0;
+    &:last-of-type {
       dd {
-        margin-left: 96px;
-        padding: 16px 15px 0 15px;
-        border-left: 1px solid #e6e6e6;
-        line-height: 24px;
-
-        .item-wrapper {
-          display: inline-block;
-          white-space: nowrap;
-        }
-        .name {
-          display: inline-block;
-          cursor: pointer;
-          &:hover {
-            text-decoration: underline;
-          }
-          &.selected {
-            background: #a7a7a7;
-            color: #fff;
-            padding: 2px 6px;
-          }
-        }
-        .divider {
-          margin: 0 8px 0 10px;
-          color: #d8d8d8;
-        }
+        padding-bottom: 25px;
       }
     }
+    dt {
+      float: left;
+      display: inline-flex;
+      align-items: center;
+      width: 70px;
+      margin: 0 -100px 0 26px;
+      padding-top: 15px;
+      font-weight: bold;
+    }
+
+    dd {
+      margin-left: 96px;
+      padding: 16px 15px 0 15px;
+      border-left: 1px solid #e6e6e6;
+      line-height: 24px;
+    }
   }
-  .footer {
-    height: 20px;
-    background-position: -1440px -12px;
+`
+
+export const PlaylistCategoryFooter = styled(Sprite).attrs({
+  sprite: 'background',
+  icon: 'footer',
+})`
+  height: 20px;
+`
+
+export const PlaylistCategoryAll = styled(Sprite).attrs({
+  sprite: 'button',
+  icon: 'greyButton',
+  component: TextNavLink,
+})`
+  display: inline-block;
+  width: 75px;
+  height: 26px;
+  line-height: 26px;
+  text-align: center;
+`
+
+const icons = ['language', 'style', 'scene', 'emotion', 'theme']
+export const PlaylistCategoryIcon = styled(Sprite).attrs(({ index }) => ({
+  sprite: 'icon',
+  icon: icons[index],
+  component: 'i',
+}))`
+  width: 23px;
+  height: 23px;
+  margin: 0 8px 4px 0;
+`
+
+export const PlaylistCategoryItem = styled.div`
+  display: inline-block;
+  white-space: nowrap;
+`
+
+export const PlaylistCategoryLink = styled(TextNavLink)`
+  display: inline-block;
+  color: #333;
+
+  &.selected {
+    background: #a7a7a7;
+    color: #fff;
+    padding: 2px 6px;
   }
 `
