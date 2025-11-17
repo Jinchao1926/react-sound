6fabd98698c44e177cb2eef55f2fe9b019d95e11
Jinchao Lin <jinchao@fireworkhq.com>
2025-11-17 15:03:11 +0800

    [Fix] Fix wrong layout + play count

--- src/utils/dataFormat.ts
+++ src/utils/dataFormat.ts
@@ -5,13 +5,20 @@ export const roundToDecimal = (num: number, decimal: number) => {
 }
 
 export const formatPlayCount = (count: number) => {
+  if (count < 100000) {
+    return count
+  } else {
+    return Math.floor(count / 10000) + '万'
+  }
+
+  /*
   if (count < 10000) {
     return count
   } else if (Math.floor(count / 10000) < 10000) {
     return Math.floor(count / 1000) / 10 + '万' // 保留一位小数
   } else {
     return Math.floor(count / 10000 / 1000) / 10 + '亿'
-  }
+  } */
 }
 
 export const formatSizedImage = (imgUrl: string, size: number = 140) => {
