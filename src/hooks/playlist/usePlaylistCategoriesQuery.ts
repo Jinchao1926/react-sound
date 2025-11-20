10b6fdce7284da937d388ab6339d33ceab245212
Jinchao Lin <jinchao@fireworkhq.com>
2025-11-20 20:12:43 +0800

    [Feature] Playlist subscribers

--- src/hooks/playlist/usePlaylistCategoriesQuery.ts
+++ src/hooks/playlist/usePlaylistCategoriesQuery.ts
@@ -4,7 +4,10 @@ import { useAxios } from '@/providers/AxiosProvider'
 import { PlaylistCategoriesApiResponse } from '@/types/playlist-category'
 import { parsePlaylistCategories } from '@/utils/playlistCategoriesParser'
 
-// 歌单分类
+/**
+ * 歌单分类
+ * 说明 : 调用此接口,可获取歌单分类,包含 category 信息
+ */
 export const usePlaylistCategoriesQuery = () => {
   const axios = useAxios()
 
