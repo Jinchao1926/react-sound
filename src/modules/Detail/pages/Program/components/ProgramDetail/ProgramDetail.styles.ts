b79990524198d101ac6636853d829ec30405674b
Jinchao Lin <jinchao.1926@gmail.com>
2025-12-01 22:50:36 +0800

    New media operation bar for program

--- src/modules/Detail/pages/Program/components/ProgramDetail/ProgramDetail.styles.ts
+++ src/modules/Detail/pages/Program/components/ProgramDetail/ProgramDetail.styles.ts
@@ -3,6 +3,29 @@ import styled from 'styled-components'
 
 import { Sprite } from '@/components/Core'
 
+export const PlayButton = styled(Sprite).attrs({
+  sprite: 'button',
+  icon: 'playBlue2',
+  component: 'button',
+})`
+  height: 31px;
+  padding: 0 7px 0 36px;
+  border-radius: 4px;
+  color: white;
+`
+
+export const LikedIcon = styled(Sprite).attrs<{ $liked: boolean }>((props) => ({
+  sprite: 'button',
+  icon: props.$liked ? 'liked' : 'like',
+  component: 'span',
+}))`
+  display: inline-block;
+  width: 17px;
+  height: 15px;
+  transform: translateY(4px);
+  margin-right: 6px;
+`
+
 export const RadioIcon = styled(Sprite).attrs({
   sprite: 'icon',
   icon: 'radioIcon',
