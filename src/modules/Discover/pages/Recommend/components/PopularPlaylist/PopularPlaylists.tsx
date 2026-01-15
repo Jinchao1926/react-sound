de6689c7cd4731f9a7c2a5769b71482328f7bc83
Jinchao Lin <jinchao.1926@gmail.com>
2026-01-15 09:06:35 +0800

    [Player] Play popular playlist

--- src/modules/Discover/pages/Recommend/components/PopularPlaylist/PopularPlaylists.tsx
+++ src/modules/Discover/pages/Recommend/components/PopularPlaylist/PopularPlaylists.tsx
@@ -2,6 +2,7 @@ import { type FC, useMemo } from 'react'
 
 import { Box } from '@/components/Core'
 import { SectionHeader } from '@/components/SectionHeader'
+import { usePlayPlaylist } from '@/hooks/player/usePlayPlaylist'
 import { usePopularPlaylistsQuery } from '@/hooks/recommend/usePopularPlaylistsQuery'
 import { PlaylistCover } from '@/modules/Discover/components/PlaylistCover'
 import { routeBuilder } from '@/routers'
@@ -12,6 +13,8 @@ import { PopularPlaylistContainer } from './PopularPlaylists.styles'
 export const PopularPlaylists: FC = () => {
   const tags = useMemo(() => ['华语', '流行', '摇滚', '民谣', '电子'], [])
   const { data } = usePopularPlaylistsQuery()
+
+  const { play } = usePlayPlaylist()
 
   return (
     <Box>
@@ -25,7 +28,11 @@ export const PopularPlaylists: FC = () => {
       />
       <PopularPlaylistContainer>
         {data.map((item) => (
-          <PlaylistCover key={item.id} playlist={item} />
+          <PlaylistCover
+            key={item.id}
+            playlist={item}
+            onPlay={() => play(item.id)}
+          />
         ))}
       </PopularPlaylistContainer>
     </Box>
