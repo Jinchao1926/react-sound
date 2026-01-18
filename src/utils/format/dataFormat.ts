f901014a7da08d0103c194352c98cffa2a06d479
Jinchao Lin <jinchao.1926@gmail.com>
2026-01-18 17:10:48 +0800

    rename function

--- src/utils/format/dataFormat.ts
+++ src/utils/format/dataFormat.ts
@@ -36,6 +36,6 @@ export const formatImage = (
   return `${imgUrl}?param=${width}y${height}`
 }
 
-export const getMusicUrl = (id: number): string => {
+export const getTrackUrl = (id: number): string => {
   return `https://music.163.com/song/media/outer/url?id=${id}.mp3`
 }
